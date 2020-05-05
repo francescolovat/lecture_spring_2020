72e4e6e18f527866fb299444fa64b29d79580fcf
Francesco Lovat <franlovat@gmail.com>
2020-05-05 15:58:53 +0200

    addition of funny2 func and respective test

--- test_utils.py
+++ test_utils.py
@@ -1,4 +1,9 @@
-from utils import funny
+from utils import funny, funny2
+
 
 def test_funny():
     assert funny(2, 1) == 3
+
+
+def test_funny2():
+    assert funny2(2, 2) == 5
--- utils.py
+++ utils.py
@@ -1,2 +1,6 @@
 def funny(x, y):
     return x + y
+
+
+def funny2(x, y):
+    return x**y
